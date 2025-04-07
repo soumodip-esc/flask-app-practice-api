6019db73e67b732744dcf29529e36ce4e17f0c7c
Soumodip Mondal <soumodipmondal40@gmail.com>
2025-04-07 21:25:42 +0530

    added cors
    a

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS  #  Import CORS
 
-# Initialize Flask App
-app = Flask(__name__)
+app = Flask(__name__) #Initialize App
+CORS(app)
 
 # Database Configuration (Update with your credentials)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
